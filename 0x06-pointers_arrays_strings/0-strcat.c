ff22e8ecbe15bc8cfd44606ef10c4cbc64a6ae85
root <audualiyuehezu>
2022-03-24 01:58:05 -0700

    Solution added for task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -4,21 +4,22 @@
  * _strcat - concatenate two strings
  * @dest: char string to concatenate to
  * @src: char string
- * return: pointer to resulting string `dest`
+ * Return: pointer to resulting string `dest`
  */
 
 char *_strcat(char *dest, char *src)
 {
-    int i,c;
+	int i, c;
 
-    for(i=0; dest[i]!= '\0'; i++);
+	for (i = 0; dest[i] != '\0'; i++)
+		;
 
-    for(c=0; src[c] != '\0'; c++)
-    {
-        dest[i] = src[c];
-        i++;
-    }
+	for (c = 0; src[c] != '\0'; c++)
+	{
+		dest[i] = src[c];
+		i++;
+	}
 
-    dest[i] = '\0';
-    return (dest);
+	dest[i] = '\0';
+	return (dest);
 }
