811d02dc563f2eb4e3d5350850702c12f373313b
root <audualiyuehezu>
2022-04-05 14:47:58 -0700

    Solution for task 2 fixed

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,48 +1,39 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "main.h"
+#include <stdlib.h>
 
 /**
- * _strlen - find length of a string
- * @s: string
- * Return: int
- */
-
-
-int _strlen(char *s)
-{
-	int size = 0;
-	for (; s[size] != '\0'; size++)
-		;
-	return (size);
-}
-
-/**
- * *str_concat - concatenates two strings
+ * str_concat - concatenate two strings using malloc
  * @s1: string 1
  * @s2: string 2
- * Return: pointer
+ * Return: pointer to concat string
  */
 
 char *str_concat(char *s1, char *s2)
 {
+	char *a;
+	int i, j, c, d;
 
-	int size1 = _strlen(s1), size2 = _strlen(s2), i;
-	char *m = malloc((size1 + size2) * sizeof(char) + 1);
-	if (m == NULL)
-		return (NULL);
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
 
-	for (i = 0; i <= size1 + size2; i++)
+	for (i = 0; s1[i] != '\0'; i++)
+		;
+	for (j = 0; s2[j] != '\0'; j++)
+		;
+
+	a = malloc((i * sizeof(*s1)) + (j * sizeof(*s2)) + 1);
+	if (a == NULL)
+		return (NULL);
+
+	for (c = 0, d = 0; c < (i + j + 1); c++)
 	{
-		if (i < size1)
-			m[i] = s1[i];
+		if (c < i)
+			a[c] = s1[c];
 		else
-			m[i] = s2[i - size1];
+			a[c] = s2[d++];
 	}
 
-	return (m);
+	return (a);
 }
